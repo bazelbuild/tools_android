a28f7c5f181747bc824b69a3056e0b15951440dd
Prasanna Swaminathan <prasanna@ergatta.com>
2024-11-11 14:30:42 -0500

    Remove hyphens from UUID to satisfy mapping file ID requirements
    Remove hyphens from UUID to satisfy mapping file ID requirements
    
    I don't know if this is official or documented anywhere, but it looks
    like the mapping file ID must be a UUID *without hyphens*. Uploading a
    file with a UUID like 6ac7912c-d163-47a0-ae4e-f6d3ad3a21c7 results in
    an API response like:
    
    ```
    "error": {
        "code": 3,
        "message": "Invalid URL: Not a valid uuid."
    }
    ```
    
    Removing the hyphens makes it work.
    
    I'm not sure how "generalizable" we want the code in this repository
    to be. It seems to me that this target should specifically be used to
    generate a UUID that can be used directly for a mapping file upload.
    To that end, I think it is correct to follow the exact format that
    makes this work, rather than make something purely generalizable.

--- tools/crashlytics/com/bazel/crashlytics/GenerateRandomUUID.java
+++ tools/crashlytics/com/bazel/crashlytics/GenerateRandomUUID.java
@@ -3,7 +3,7 @@ package com.bazel.crashlytics;
 public class GenerateRandomUUID {
 
   public static void main(String[] args) {
-    System.out.println(java.util.UUID.randomUUID().toString());
+    System.out.println(java.util.UUID.randomUUID().toString().replace("-", ""));
   }
 
 }
